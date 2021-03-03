0e6abec5fc0f2dbf93661f8b25142892626a6ff6
Garrett Bjerkhoel <me@garrettbjerkhoel.com>
2021-03-02 21:38:57 -0800

    Remove application key

--- config/initializers/cased.rb
+++ config/initializers/cased.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 Cased.configure do |config|
-  config.guard_application_key = ''
+  # config.guard_application_key = ''
   # config.filter_parameters = true
   # config.unfiltered_parameters = [
   #   # Database record ID's
