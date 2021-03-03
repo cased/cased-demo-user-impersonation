14b9fd49755c765369d26cbc2468547ad1295979
Garrett Bjerkhoel <me@garrettbjerkhoel.com>
2021-03-02 21:02:28 -0800

    Setup Cased session approval flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Cased::Rails::Engine => '/cased'
+
   post '/login/:user_id' => 'sessions#create', as: :login
   post '/impersonate/:user_id' => 'sessions#impersonate', as: :impersonate
   delete '/logout' => 'sessions#destroy', as: :logout
